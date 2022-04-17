89db71e11d7c39d1fb888d75ac3491c80b9b81e5
Tran Ngoc Lai <lai.tran@executionlab.asia>
2022-04-17 14:18:01 +0700

    Update Config For Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Tranlight
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoloader = :classic
+
 
     # Configuration for the application, engines, and railties goes here.
     #
